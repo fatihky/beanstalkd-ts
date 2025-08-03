c144790365e2e7a832990ca72d283215187a8b69
Fatih Kaya <1994274@gmail.com>
2025-08-03 16:35:23 +0300

    bench: measure producer performance too

--- bench/producer.ts
+++ bench/producer.ts
@@ -1,11 +1,24 @@
 import { BeanstalkdClient } from '../src';
 
+let processed = 0;
+let lastChecked = Date.now();
+
+setInterval(() => {
+  const throughput = (processed * 1000) / (Date.now() - lastChecked);
+
+  console.log(`producer throughput: ${throughput.toFixed(0)} jobs/s`);
+
+  // reset
+  lastChecked = Date.now();
+  processed = 0;
+}, 10000);
+
 async function producer() {
   const client = new BeanstalkdClient();
 
   await client.connect();
 
-  for (;;) {
+  for (; ; processed++) {
     await client.put('test payload');
   }
 }
